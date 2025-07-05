a0983623a1e90ec24f4df06d6856f1886454add4
ssentongo trevor <ssentongotrevor2003@outlook.com>
2025-07-05 13:37:30 +0300

    Create No.3(c)using While loop.java

---
+++ No.3(c)using While loop.java
@@ -0,0 +1,52 @@
+public class PM25TrackingWhile {
+    public static void main(String[] args) {
+        int day = 1;
+        int[] pm25Levels = new int[30];
+
+        while (day <= 30) {
+            // Simulate PM2.5 level for thepublic class PM25TrackingWhile {
+    public static void main(String[] args) {
+        int day = 1;
+        int[] pm25Levels = new int[30];
+
+        while (day <= 30) {
+            // Simulate PM2.5 level for the day (random value for demonstration)
+            pm25Levels[day - 1] = (int) (Math.random() * 300);
+            System.out.println("Day " + day + ": PM2.5 Level = " + pm25Levels[day - 1]);
+            day++;
+        }
+    }
+}
+public class PM25TrackingWhile {
+    public static void main(String[] args) {
+        int day = 1;
+        int[] pm25Levels = new int[30];
+
+        while (day <= 30) {
+            // Simulate PM2.5 level for the day (random value for demonstration)
+            pm25Levels[day - 1] = (int) (Math.random() * 300);
+            System.out.println("Day " + day + ": PM2.5 Level = " + pm25Levels[day - 1]);
+            day++;
+        }
+    }
+}
+ day (random value for demonstration)
+            pm25Levels[day - 1] = (int) (Math.random() * 300);
+            System.out.println("Day " + day + ": PM2.5 Level = " + pm25Levels[day - 1]);
+            day++;
+        }
+    }
+}
+public class PM25TrackingWhile {
+    public static void main(String[] args) {
+        int day = 1;
+        int[] pm25Levels = new int[30];
+
+        while (day <= 30) {
+            // Simulate PM2.5 level for the day (random value for demonstration)
+            pm25Levels[day - 1] = (int) (Math.random() * 300);
+            System.out.println("Day " + day + ": PM2.5 Level = " + pm25Levels[day - 1]);
+            day++;
+        }
+    }
+}
